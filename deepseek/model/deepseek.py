feb8687a37bd6e036524610360d3410319cb74d1
Aakash Thatte <aathatte2002@gmail.com>
2025-09-20 16:15:03 +0530

    implement grouped query attention

--- deepseek/model/deepseek.py
+++ deepseek/model/deepseek.py
@@ -10,6 +10,10 @@ class DeepSeekModelConfig:
     embed_dim: int = 512
     bias: bool = False
     use_mla: bool = False
+
+    kv_heads: int = (
+        4  # number of groups of attention heads that share the same K and V matrices
+    )
     pass
 
 
@@ -111,11 +115,70 @@ class MultiQueryAttention(nn.Module):
         return out
 
 
+class GroupedQueryAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.num_heads = config.num_attention_heads
+        self.input_dim = config.input_dim
+        self.embed_dim = config.embed_dim
+        self.head_dim = self.embed_dim // self.num_heads
+        self.kv_heads = config.kv_heads
+
+        self.Wq = nn.Linear(self.input_dim, self.embed_dim, bias=False)
+        self.Wk = nn.Linear(self.input_dim, self.head_dim * config.kv_heads, bias=False)
+        self.Wv = nn.Linear(self.input_dim, self.head_dim * config.kv_heads, bias=False)
+        self.out_proj = nn.Linear(self.embed_dim, self.input_dim, bias=config.bias)
+
+    def forward(self, x):
+        batch_size, num_tokens, input_dim = x.shape
+        Q = (
+            self.Wq(x)
+            .view(batch_size, num_tokens, self.num_heads, self.head_dim)
+            .transpose(1, 2)
+        )  # becomes B, num_heads, T, head_dim
+
+        K = self.Wk(x)  # B, T, head_dim*kv_heads
+        V = self.Wv(x)  # B, T, head_dim*kv_heads
+
+        K = K.view(batch_size, num_tokens, self.kv_heads, self.head_dim)
+        V = V.view(batch_size, num_tokens, self.kv_heads, self.head_dim)
+
+        # now i need this
+        # if kv_heads is 3 and num_heads is 6
+        # I want k = [k1, k1, k2, k2, k3, k3] and same for v
+        K = K.repeat_interleave(
+            self.num_heads // self.kv_heads, dim=2
+        )  # B, T, num_heads, head_dim
+        V = V.repeat_interleave(
+            self.num_heads // self.kv_heads, dim=2
+        )  # B, T, num_heads, head_dim
+
+        attention_scores = Q @ K.transpose(2, 3)
+        attention_scores = attention_scores / (self.head_dim**0.5)
+
+        causal_mask = torch.triu(torch.ones(num_tokens, num_tokens), diagonal=1)
+
+        attention_scores = attention_scores.masked_fill(
+            causal_mask.bool(), float("-inf")
+        )
+        attention_weights = torch.softmax(
+            attention_scores, dim=-1
+        )  # B, num_heads, T, T
+
+        context = attention_weights @ V  # B, num_heads, T, head_dim
+        context = attention_weights.transpose(1, 2)  # B, T, num_heads, head_dim
+        context = attention_weights.view(batch_size, num_tokens, self.embed_dim)
+        out = self.out_proj(context)  # B, T, input_dim
+        return out
+
+
 if __name__ == "__main__":
     x = torch.rand(1, 2, 3)
     config = DeepSeekModelConfig()
     mha = MultiHeadAttention(config)
     mqa = MultiQueryAttention(config)
+    gqa = GroupedQueryAttention(config)
 
     print(sum(p.numel() for p in mha.parameters()))
     print(sum(p.numel() for p in mqa.parameters()))
+    print(sum(p.numel() for p in gqa.parameters()))
